155bfc714f6ff881ab6993b9901d5ecfb253d52a
ogvirtan <ovirtanen96@gmail.com>
2025-04-16 16:56:45 +0300

    movelist korjaus

--- src/game/shakki.py
+++ src/game/shakki.py
@@ -622,29 +622,15 @@ class Shakki:
     def can_bishop_movelist(self,x,y,board,mover):
         movelist = []
         n = len(board)
-        unblockeddiagonals = [True,True,True,True]
         for dz in range(1,n):
-            if unblockeddiagonals[0]:
-                if self.preview_move(x,y,dz,dz,board,mover):
-                    movelist.append(self.move_as_UCI(x,y,dz,dz))             
-                else:
-                    unblockeddiagonals[0] = False
-            if unblockeddiagonals[1]:
-                if self.preview_move(x,y,dz,-dz,board,mover):
-                    movelist.append(self.move_as_UCI(x,y,dz,-dz))
-                else:
-                    unblockeddiagonals[1] = False
-            if unblockeddiagonals[2]:
-                if self.preview_move(x,y,-dz,dz,board,mover):
-                    movelist.append(self.move_as_UCI(x,y,-dz,dz)) 
-                else:
-                    unblockeddiagonals[2] = False
-            if unblockeddiagonals[3]:
-                if self.preview_move(x,y,-dz,-dz,board,mover):
-                    movelist.append(self.move_as_UCI(x,y,-dz,-dz))
-                else:      
-                    unblockeddiagonals[3] = False
-            else:
+            if self.preview_move(x,y,dz,dz,board,mover):
+                movelist.append(self.move_as_UCI(x,y,dz,dz))             
+            if self.preview_move(x,y,dz,-dz,board,mover):
+                movelist.append(self.move_as_UCI(x,y,dz,-dz))
+            if self.preview_move(x,y,-dz,dz,board,mover):
+                movelist.append(self.move_as_UCI(x,y,-dz,dz)) 
+            if self.preview_move(x,y,-dz,-dz,board,mover):
+                movelist.append(self.move_as_UCI(x,y,-dz,-dz))
                 break
         return movelist
 
@@ -664,8 +650,25 @@ class Shakki:
 
     def can_queen_movelist(self,x,y,board,mover):
         movelist = []
-        movelist.extend(self.can_bishop_movelist(x,y,board,mover))
-        movelist.extend(self.can_rook_movelist(x,y,board,mover))
+        unblockeddiagonals = [True, True, True, True]
+        n = len(board)
+        for dz in range(1,n):
+            if self.preview_move(x,y,dz,0,board,mover):
+                movelist.append(self.move_as_UCI(x,y,dz,0)) 
+            if self.preview_move(x,y,0,-dz,board,mover):
+                movelist.append(self.move_as_UCI(x,y,0,-dz)) 
+            if self.preview_move(x,y,-dz,0,board,mover):
+                movelist.append(self.move_as_UCI(x,y,-dz,0)) 
+            if self.preview_move(x,y,0,dz,board,mover):
+                movelist.append(self.move_as_UCI(x,y,0,dz))
+            if self.preview_move(x,y,dz,dz,board,mover):
+                movelist.append(self.move_as_UCI(x,y,dz,dz))             
+            if self.preview_move(x,y,dz,-dz,board,mover):
+                movelist.append(self.move_as_UCI(x,y,dz,-dz))
+            if self.preview_move(x,y,-dz,dz,board,mover):
+                movelist.append(self.move_as_UCI(x,y,-dz,dz)) 
+            if self.preview_move(x,y,-dz,-dz,board,mover):
+                movelist.append(self.move_as_UCI(x,y,-dz,-dz))
         return movelist
     
     def can_king_movelist(self,x,y,board,mover):
@@ -763,14 +766,36 @@ class Shakki:
         n = len(board)
         unblockeddiagonals = [True,True,True,True]
         for dz in range(1,n):
-            if self.square_is_in_bounds(x+dz,y+dz):
-                movelist.append(self.position_after_move_as_tuple(x,y,dz,dz))
-            if self.square_is_in_bounds(x+dz,y-dz):
-                movelist.append(self.position_after_move_as_tuple(x,y,dz,-dz))
-            if self.square_is_in_bounds(x-dz,y+dz):
-                movelist.append(self.position_after_move_as_tuple(x,y,-dz,dz))
-            if self.square_is_in_bounds(x-dz,y-dz):
-                movelist.append(self.position_after_move_as_tuple(x,y,-dz,-dz)) 
+            if unblockeddiagonals[0]:
+                    if self.square_is_in_bounds(x+dz,y+dz):
+                        movelist.append(self.position_after_move_as_tuple(x,y,dz,dz))
+                        if self.choose_square(x+dz,y+dz,board) != 0:
+                            unblockeddiagonals[0] = False             
+                    else:
+                        unblockeddiagonals[0] = False
+            if unblockeddiagonals[1]:
+                if self.square_is_in_bounds(x+dz,y-dz):
+                    movelist.append(self.position_after_move_as_tuple(x,y,dz,-dz))
+                    if self.choose_square(x+dz,y-dz,board) != 0:
+                        unblockeddiagonals[1] = False    
+                else:
+                    unblockeddiagonals[1] = False
+            if unblockeddiagonals[2]:
+                if self.square_is_in_bounds(x-dz,y+dz):
+                    movelist.append(self.position_after_move_as_tuple(x,y,-dz,dz))
+                    if self.choose_square(x-dz,y+dz,board) != 0:
+                        unblockeddiagonals[2] = False    
+                else:
+                    unblockeddiagonals[2] = False
+            if unblockeddiagonals[3]:
+                if self.square_is_in_bounds(x-dz,y-dz):
+                    movelist.append(self.position_after_move_as_tuple(x,y,-dz,-dz))
+                    if self.choose_square(x-dz,y-dz,board) != 0:
+                        unblockeddiagonals[3] = False    
+                else:      
+                    unblockeddiagonals[3] = False
+            else:
+                break
         return movelist
     
     def rook_control(self,x,y,board):
